0c544cca550a42ace2f717ab2b5abada3da45fac
Pedro_Costa <pedro.junior@sanp.com.br>
2026-05-08 17:21:43 -0300

    IMPORTANT CHANGE: A new branch named menu has been created; it contains the files related to the menu creation code for the user, however, some corrections still need to be made for better performance.

--- main.py
+++ main.py
@@ -2,33 +2,51 @@ def main():
     tasks = []
 
     while True:
-        print("\n--- NOVA TAREFA ---")
 
-        name = input("Qual a tarefa? ")
-        status = input("Status: ")
-        responsible = input("Responsável: ")
+        print("\n======= Seja Bem Vindo =======\n"
+        "\n1 - Adicionar Tarefa.\n" 
+        "2 - Deletar Tarefa.\n" 
+        "3 - Editar Tarefa.\n" 
+        "4 - Listar Tarefas.\n"
+        "5 - Sair!\n")
 
-        task = {
-            "name": name,
-            "status": status,
-            "responsible": responsible
-        }
+        option = input("Escolha uma opção:")
 
-        tasks.append(task)
+        if option == "1": 
 
-        continuar = input("\nDeseja adicionar outra tarefa? (sim/não): ").strip().lower()
+            print("\n--- NOVA TAREFA ---")
 
-        if continuar != "sim":
-            break
+            name = input("Qual a tarefa? ")
+            status = input("Status: ")
+            responsible = input("Responsável:")
 
-    print("\n------- TODAS AS TAREFAS -------")
+            task = {
+                "name": name,
+                "status": status,
+                "responsible": responsible
+            }
 
-    for i, task in enumerate(tasks, start=1):
-        print(f"\nTarefa {i}")
-        print("Nome:", task["name"])
-        print("Status:", task["status"])
-        print("Responsável:", task["responsible"])
+            tasks.append(task)
+
+            continuar = input("\nDeseja adicionar outra tarefa? (sim/não): ").strip().lower()
+
+            if continuar != "sim":
+                break
+
+        if option == "4": 
+
+            print("\n------- TODAS AS TAREFAS -------")
+
+            for i, task in enumerate(tasks, start=1):
+                print(f"\nTarefa {i}")
+                print("Nome:", task["name"])
+                print("Status:", task["status"])
+                print("Responsável:", task["responsible"])
 
 
 if __name__ == "__main__":
     main()
+
+    # numero 1 está funcioando perfeitamente porém na linha 31 ainda está errado a solicitaçao ao usuário, precisa concertar
+    # numero 4 está lisantando certinho as atividades que foram criadas porém ele ainda está imprimindo na tela o menu novamente, precisa ser concertado. 
+    
