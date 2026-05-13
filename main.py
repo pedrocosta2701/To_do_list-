f9d75ee950dd27ba4975feeacacda856f6acadc0
Pedro_Costa <pedro.junior@sanp.com.br>
2026-05-12 21:22:13 -0300

    New:Additional function modules for each menu item.

--- main.py
+++ main.py
@@ -1,46 +1,74 @@
-def main():
-    tasks = []
-
-    while True:
-
-        print("\n======= Seja Bem Vindo =======\n"
-        "\n1 - Adicionar Tarefa.\n" 
-        "2 - Deletar Tarefa.\n" 
-        "3 - Editar Tarefa.\n" 
+def menu():
+     
+     print("\n ----  Seja Bem Vindo  ----  \n"
+        "\n1 - Adicionar Tarefa.\n"
+        "2 - Deletar Tarefa.\n"
+        "3 - Editar Tarefa.\n"
         "4 - Listar Tarefas.\n"
-        "5 - Sair!\n")
+        "5 - Sair!\n"
+     )
 
-        option = input("Escolha uma opção:")
+def add_task(tasks):
 
-        if option == "1": 
+        print("----| Adicione uma nova tarefa |----\n")
 
-            print("\n--- NOVA TAREFA ---")
+        task_name = input("Qual a tarefa:")
+        task_status = input("Status:")
+        task_responsible = input("Reponsável:")
 
-            name = input("Qual a tarefa? ")
-            status = input("Status: ")
-            responsible = input("Responsável:")
+        task = {
+             "name": task_name,
+             "status": task_status,
+             "responsible": task_responsible 
+        }
 
-            task = {
-                "name": name,
-                "status": status,
-                "responsible": responsible
-            }
+        tasks.append(task)
 
-            tasks.append(task)
+        print(f"\n Tarefa {task_name} adicionada! ")
 
-            print("\nTarefa adicionada.")
-                
-          
-        if option == "4": 
+def list_tasks(tasks):
+   
+        print("\n------- TODAS AS TAREFAS -------")
 
-            print("\n------- TODAS AS TAREFAS -------")
+        if not tasks: 
+             
+             print("Nenhuma tarefa cadastrada")
 
-            for i, task in enumerate(tasks, start=1):
+        else:
+             
+            for i, task in enumerate(tasks, start=0):
+                print("| ===================== |")
                 print(f"\nTarefa {i}")
-                print("Nome:", task["name"])
-                print("Status:", task["status"])
-                print("Responsável:", task["responsible"])
+                print("| ===================== |")
+                print("\nNome:", task["name"])
+                print("| ===================== |")
+                print("\nStatus:", task["status"])
+                print("| ===================== |")
+                print("\nResponsável:", task["responsible"])
+                print("| ===================== |")
 
+def main():
+        
+        tasks = []
+
+        while True:
+
+            menu()
+
+            option = input("Escolha uma opção:\n")
+
+            if option == "1": 
+                 add_task(tasks)
+            
+            elif option == "4": 
+                 list_tasks(tasks)
+
+            elif option == "5":
+                 print("Até logo")
+                 break
+            
+            else: 
+                 print("Opção inválida")
 
 if __name__ == "__main__":
     main()
